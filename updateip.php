2d2b2c07d6710c64c26ce15addeeaa851121acef
skytap <skytap@host-1.skytap.example>
2016-09-12 12:43:40 -0700

    adding updateip.php file

---
+++ updateip.php
@@ -0,0 +1,26 @@
+<?php
+$allowedIPs = ["123.123.123.123","12.12.12.12"];
+$referer = $_SERVER['HTTP_REFERER'];
+$ip = $_POST["ip"];
+
+//db info file
+include("./db.php");
+
+//see if IP requesting is allowed
+if(in_array($SERVER['HTTP_REFERER'],$allowedIPs)) {
+	//set in db
+	$conn = new mysqli($host,$username,$password,$database);
+	if(!$conn) {
+		die("Connection Error: " . mysqli_connect_error());
+	} else {
+		$sql = "INSERT INTO $table (ip, created) VALUES ($ip,NOW())";
+		if(mysqli_query($conn, $sql)) {
+			echo "success";
+		} else {
+			echo "FAIL";
+		}
+	}
+	$conn->close();	
+}
+
+?>
