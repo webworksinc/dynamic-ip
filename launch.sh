28d532c378b0e0d55f45c533ea47c2b06bb09e59
webworksinc <webworksinc@gmail.com>
2016-09-14 13:02:45 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo $ip
 
 #send IP to script
-ip=$ip; curl -X GET "http://www.webworksmd.com/addip.php${ip}"
+ip=$ip; curl -X GET "http://www.webworksmd.com/updateip.php${ip}"
